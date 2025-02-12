d559433854dc31732b9423c3a212c08aa8ac87f1
yeniObabatunde <babatundeabike3@gmail.com>
2025-02-13 00:05:02 +0100

    folder_set_up

---
+++ RickndMortyList/RickndMortyList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickndMortyList
+//
+//  Created by Sharon Omoyeni Babatunde on 12/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickndMortyList/RickndMortyList/RickndMortyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickndMortyListApp.swift
+//  RickndMortyList
+//
+//  Created by Sharon Omoyeni Babatunde on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickndMortyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
