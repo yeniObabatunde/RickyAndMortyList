ec3dba776d743830a7270591aad761c382cd3de9
yeniObabatunde <babatundeabike3@gmail.com>
2025-02-13 01:12:39 +0100

    Create AppConstants.swift

---
+++ RickndMortyList/RickndMortyList/Utils/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  RickndMortyList
+//
+//  Created by Sharon Omoyeni Babatunde on 13/02/2025.
+//
+
+import Foundation
+
+enum AppConstants {
+    
+  enum Api {
+        static let baseUrlString = "https://rickandmortyapi.com/api/"
+      static let character = "character"
+    }
+}
