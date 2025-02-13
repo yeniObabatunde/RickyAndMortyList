889a8481d0cb3b7800fe48b2b6d1980259812542
yeniObabatunde <babatundeabike3@gmail.com>
2025-02-13 10:39:57 +0100

    Update NetworkError.swift

--- RickndMortyList/RickndMortyList/Network/NetworkError.swift
+++ RickndMortyList/RickndMortyList/Network/NetworkError.swift
@@ -7,13 +7,29 @@
 
 import Foundation
 
-enum NetworkError: Error {
+enum NetworkError: Error, Equatable {
     case invalidURL
     case noData
     case decodingError
     case serverError(Int)
     case unknown(Error)
+    
+    static func == (lhs: NetworkError, rhs: NetworkError) -> Bool {
+        switch (lhs, rhs) {
+        case (.invalidURL, .invalidURL),
+             (.noData, .noData),
+             (.decodingError, .decodingError):
+            return true
+        case let (.serverError(code1), .serverError(code2)):
+            return code1 == code2
+        case (.unknown, .unknown):
+            return false 
+        default:
+            return false
+        }
+    }
 }
+
 
 
 enum HTTPMethod: String {
